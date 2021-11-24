25adb9e2f41e0c74e5cf282508b4ad655f35c16c
libsonnPolsl <wojclib126@student.polsl.pl>
2021-11-24 20:30:03 +0100

    Added hud with values, TODO add players Data

---
+++ GR_Projekt/States/Game/HUD/HUDCell.cs
@@ -0,0 +1,21 @@
+﻿
+
+using GR_Projekt.Core;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GR_Projekt.States.Game
+{
+    abstract public class HUDCell
+    {
+        protected Rectangle _cellRectangle;
+
+        public HUDCell(Rectangle cell)
+        {
+            this._cellRectangle = cell;
+        }
+
+        abstract public void Update(string updatedValue);
+        abstract public void Draw(GameTime gameTime, SpriteBatch spriteBatch);
+    }
+}
---
+++ GR_Projekt/States/Game/HUD/HUDDoomFaceCell.cs
@@ -0,0 +1,30 @@
+﻿using GR_Projekt.Content.Images;
+using GR_Projekt.Content.Images.Controls;
+using GR_Projekt.Core;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GR_Projekt.States.Game.HUD
+{
+    public class HUDDoomFaceCell : HUDCell
+    {
+        private ImageComponent _doomFace;
+
+        public HUDDoomFaceCell(ContentManager contentManager, Rectangle cell) : base(cell: cell)
+        {
+
+            this._doomFace = new ImageComponent(contentManager, MainMenuImages.menuDoomFace, cell.Center.ToVector2(), cell.Width / 2 - Paddings.componentHorizontalPadding * 2, cell.Height / 1 - Paddings.componentHorizontalPadding * 2);
+        }
+
+        public override void Update(string updatedValue)
+        {
+
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            _doomFace.Draw(gameTime, spriteBatch);
+        }
+    }
+}
